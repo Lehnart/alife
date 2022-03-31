7b94f08ac28607cb2a15aea04927bfb9137481de
Lehnart <thomasserre63@gmail.com>
2022-03-31 22:04:45 +0200

    Document array data structure

--- src/tools/array.c
+++ src/tools/array.c
@@ -2,6 +2,11 @@
 #include <string.h>
 #include "array.h"
 
+/**
+ * Allocate a new array.
+ * @param size initial size of the array
+ * @return the newly allocated array
+ */
 Array *array_new(unsigned int size) {
     Array *array = malloc(sizeof(Array));
     array->size = size;
@@ -10,6 +15,12 @@ Array *array_new(unsigned int size) {
     return array;
 }
 
+/**
+ * Get the element at the given index. If the index is invalid, NULL is returned.
+ * @param array
+ * @param index
+ * @return element at the given index
+ */
 void* array_get(const Array *array, unsigned int index) {
     if (index >= array->size) {
         return NULL;
@@ -18,6 +29,12 @@ void* array_get(const Array *array, unsigned int index) {
     return array->data_array[index];
 }
 
+/**
+ * Set the element at the given index. If the index is invalid, nothing is done.
+ * @param array
+ * @param index
+ * @param data
+ */
 void array_set(Array *array, unsigned int index, void *data) {
     if (index >= array->size) {
         return;
@@ -25,7 +42,12 @@ void array_set(Array *array, unsigned int index, void *data) {
     array->data_array[index] = data;
 }
 
-
+/**
+ * Append an element at the end of the array. If needed, memory is allocated.
+ * @param array
+ * @param index
+ * @param data
+ */
 void array_append(Array *array, void *data) {
     if (array->size + 1 > array->memory_size) {
         void **new_data_array = malloc(2 * array->memory_size * sizeof(void *));
@@ -39,6 +61,10 @@ void array_append(Array *array, void *data) {
     array->data_array[array->size - 1] = data;
 }
 
+/**
+ * Clear the array, not freeing element memories.
+ * @param array
+ */
 void array_clear(Array *array){
     array->size = 0;
 }
--- src/tools/array.h
+++ src/tools/array.h
@@ -1,20 +1,20 @@
 #ifndef ALIFE_ARRAY_H
 #define ALIFE_ARRAY_H
 
-typedef struct array{
-    unsigned int size;
-    unsigned int memory_size;
-    void **data_array;
+/**
+ * Dynamic array data structure.
+ * Each element is a void*.
+ */
+typedef struct Array{
+    unsigned int size;          // Number of element present in array
+    unsigned int memory_size;   // Number of memory block allocated for element
+    void **data_array;          // Pointer to array
 } Array;
 
-Array *array_new(unsigned int size);
-
-void* array_get(const Array *array, unsigned int index);
-
-void array_set(Array *array, unsigned int index, void *data);
-
-void array_append(Array *array, void *data);
-
-void array_clear(Array *array);
+Array*      array_new       (unsigned int size);
+void*       array_get       (const Array *array, unsigned int index);
+void        array_set       (Array *array, unsigned int index, void *data);
+void        array_append    (Array *array, void *data);
+void        array_clear     (Array *array);
 
 #endif //ALIFE_ARRAY_H
